6f9c9c692b8aa24a275b9ac9ebca771bfb5caf80
Anne <95251413+AnneH20@users.noreply.github.com>
2024-09-19 08:02:38 -0500

    adding repository annotions to repo classes

--- src/main/kotlin/com/example/blog/repository/ArticleRepository.kt
+++ src/main/kotlin/com/example/blog/repository/ArticleRepository.kt
@@ -2,7 +2,9 @@ package com.example.blog.repository
 
 import com.example.blog.model.ArticleDo
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ArticleRepository : CrudRepository<ArticleDo, Long> {
     fun findBySlug(slug: String): ArticleDo?
 
--- src/main/kotlin/com/example/blog/repository/UserRepository.kt
+++ src/main/kotlin/com/example/blog/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.example.blog.repository
 
 import com.example.blog.model.UserDo
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : CrudRepository<UserDo, Long> {
     fun findByUsername(username: String): UserDo?
 }
